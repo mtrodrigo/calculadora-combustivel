f3436d2c92b3d1e74b49ff831293cd526fe45065
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-01-29 19:16:23 -0300

    Finalizado o desingn da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-
+import Logo from './assets/logo.png'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <main className='calculadora' >
+        <section className='container'>  
+          <img src={Logo} alt="Logo" />
+          <h1>CALCULADORA DE ALCOOL OU GASOLINA</h1>
+          <p>Calcula pelo preço se é melhor alcool ou gasolina</p>
+          <input
+            
+            placeholder='Valor do alcool (0,00)'
+            required
+          />
+          <input
+            placeholder='Valor da gasolina (0,00)'
+            required
+          />
+          <button>Calcular</button>
+          <h3>%</h3>
+          <h3>Melhor combustivel para abastecer:</h3> 
+        </section>
+      </main>  
     </>
   )
 }
